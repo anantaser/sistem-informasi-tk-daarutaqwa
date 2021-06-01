1e84a83b51e54072ede1651a22bbabe795fe0cd4
anantaser <ananta.satria@gmail.com>
2021-06-02 00:03:13 +0700

    koneksi ppdb dengan sql

--- functions.php
+++ functions.php
@@ -49,5 +49,52 @@ function registrasi($data){
 	return mysqli_affected_rows($conn);
 }
 
+function ppdb($datappdb){
+	global $conn;
+	echo "Berhasil trigger button";
+	$nama = mysqli_real_escape_string($conn,$datappdb["nama"]);
+	$almtlkp = mysqli_real_escape_string($conn, $datappdb["almtlkp"]);
+	$jk = mysqli_real_escape_string($conn, $datappdb["jk"]);
+	$tglbsm = mysqli_real_escape_string($conn, $datappdb["tglbsm"]);
+	$nik = mysqli_real_escape_string($conn, $datappdb["nik"]);
+	$anakke = mysqli_real_escape_string($conn, $datappdb["anakke"]);
+	$tempatlahir = mysqli_real_escape_string($conn, $datappdb["tempatlahir"]);
+	$tanggallahir = mysqli_real_escape_string($conn, $datappdb["tanggallahir"]);
+	$usia = mysqli_real_escape_string($conn, $datappdb["usia"]); 
+	$agm = mysqli_real_escape_string($conn, $datappdb["agm"]);
+	$nhp = mysqli_real_escape_string($conn, $datappdb["nhp"]);
+	$kwg = mysqli_real_escape_string($conn, $datappdb["kwg"]);
+	// break section
+	$namaayah = mysqli_real_escape_string($conn, $datappdb["namaayah"]);
+	$pdkayah = mysqli_real_escape_string($conn, $datappdb["pdkayah"]);
+	$nikayah = mysqli_real_escape_string($conn, $datappdb["nikayah"]);
+	$pkjayah = mysqli_real_escape_string($conn, $datappdb["pkjayah"]);
+	$tglayah = mysqli_real_escape_string($conn, $datappdb["tglayah"]);
+	// break section
+	$namaibu = mysqli_real_escape_string($conn, $datappdb["namaibu"]);
+	$pdkibu = mysqli_real_escape_string($conn, $datappdb["pdkibu"]);
+	$nikibu = mysqli_real_escape_string($conn, $datappdb["nikibu"]);
+	$pkjibu = mysqli_real_escape_string($conn, $datappdb["pkjibu"]);
+	$tglibu = mysqli_real_escape_string($conn, $datappdb["tglibu"]);
+	// Break Section
+	$tgbdn = mysqli_real_escape_string($conn, $datappdb["tgbdn"]);
+	$jktp = mysqli_real_escape_string($conn, $datappdb["jktp"]);
+	$btbdn = mysqli_real_escape_string($conn, $datappdb["btbdn"]);
+	$js = mysqli_real_escape_string($conn, $datappdb["js"]);
+	// Break Section
+	$jenis = mysqli_real_escape_string($conn, $datappdb["jenis"]);
+	$rombel = mysqli_real_escape_string($conn, $datappdb["rombel"]);
+	$rombel = mysqli_real_escape_string($conn, $datappdb["rombel"]);
+	$tglmskskl = mysqli_real_escape_string($conn, $datappdb["tglmskskl"]);
+	$nis = mysqli_real_escape_string($conn, $datappdb["nis"]);
+
+	$sql1 = "INSERT INTO `ppdb`(`id_ppdb`, `namalengkap`, `jk`, `nik`, `tl_anak`, `agama_anak`, `kwn_anak`, `alamat`, `tinggal_bersama`, `anak_ke`, `usia_anak`, `no_hp`, `nama_ayah`, `nik_ayah`, `ttl_ayah`, `pend_ayah`, `pek_ayah`, `nama_ibu`, `nik_ibu`, `ttl_ibu`, `pend_ibu`, `pek_ibu`, `tinggi_badan`, `berat_badan`, `jarak_tempuh`, `jumlah_saudara`, `jenis_pendaftaran`, `tanggal_masuk`, `masuk_rombel`, `status_setuju`) VALUES ('','$nama','$jk','$nik','$tempatlahir','$tglbsm','$anakke','$usia','$nhp','$namaayah','$nikayah','$tglayah','$pdkayah','$pkjayah','$namaibu','$nikibu','$tglibu','$pdkibu','$pkjibu','$tgbdn','$btbdn','$jktp','$js','$jenis','$','[value-26]','[value-27]','[value-28]','[value-29]','[value-30]')"
+
+	// variable value ada yang terlewat mohon double cek syntax sqlnya
+
+	mysqli_query($conn,"");
+	return mysqli_affected_rows($conn);
+	}
+
 
  ?>
--- ppdb.php
+++ ppdb.php
@@ -1,268 +1,256 @@
+<?php 
+
+require 'functions.php';
+
+if (isset($_POST["ppdb"])){
+
+	if( ppdb($_POST) > 0 ) {
+		echo "<script>
+				alert('data berhasil ditambahkan!');
+		</script>";
+	} else {
+		echo mysqli_error($conn);
+	}
+}
+
+ ?>
+
 <!DOCTYPE html>
 <html>
-<head>
-<title>Halaman PPDB</title>
-<link rel="stylesheet" href="css/ppdb.css">
+	<head>
+		<title>Halaman PPDB</title>
+		<link rel="stylesheet" href="css/ppdb.css">
 
-<div class="header">
-  <img src="assets/logotk.jpg" width="200">
-  <h1>TK Islam Daarutaqwa</h1>
-  <p>Jl. Raya bogor, KM. 44, Pakansari, Cibinong, Bogor, West Java 16915</p>
-</div>
+		<div class="header">
+		  <img src="assets/logotk.jpg" width="200">
+		  <h1>TK Islam Daarutaqwa</h1>
+		  <p>Jl. Raya bogor, KM. 44, Pakansari, Cibinong, Bogor, West Java 16915</p>
+		</div>
 
-<div class="topnav">
-  <a href="index.php">Mengenai Kita</a>
-  <a href="fotokegiatan.php">Foto Kegiatan</a>
-  <a href="ppdb.php">PPDB</a>
-  <a href="login.php" style="float:right" >Masuk/Login</a>
-</div>
-
-</head>
-
+		<div class="topnav">
+		  <a href="index.php">Mengenai Kita</a>
+		  <a href="fotokegiatan.php">Foto Kegiatan</a>
+		  <a href="ppdb.php">PPDB</a>
+		  <a href="login.php" style="float:right" >Masuk/Login</a>
+		</div>
+	</head>
 
 <body>
 
-<h1>Halaman PPDB</h1>
+	<h1>Halaman PPDB</h1>
 
+		<form action="" method="post">
 
- <div class="container">	
- 	<div class="title">A. Indentitas Calon Siswa/i</div>
+		 <div class="container">	
+ 			<div class="title">A. Indentitas Calon Siswa/i</div>
 	  	<div class="content">
 	   	   	<form action="#">
-		  		<div class="user-details">
+		  			<div class="user-details">
+		  				<br><br>
 		    		<div class="input-box">
-					  <span class="details">Nama Lengkap </span>
-					  <input type="text" placeholder="" name="nama" id="nama">
+					  	<span class="details">Nama Lengkap </span>
+					  	<input type="text" placeholder="" name="nama" id="nama">
 		    		</div>
-		    
-					<div class="input-box">
+
+						<div class="input-box">
+							<span class="details">Alamat Lengkap </span>
+							<input type="text" placeholder="" 	="almtlkp" id="almtlkp">
+						</div>
+			
+						<div class="input-box">
+							<span class="details">Jenis Kelamin </span>
+							<input type="text" placeholder=""name="jk" id="jk">
+						</div>			
+			
+						<div class="input-box">
+							<span class="details">Tinggal Bersama</span>
+							<input type="text" placeholder=""name="tglbsm" id="tglbsm">
+						</div>
 				
-						<span class="details">Alamat Lengkap </span>
-						<input type="text" placeholder="" name="almtlkp" id="almtlkp">
-						
-					</div>
-			
-					<div class="input-box">
-						<span class="details">Jenis Kelamin </span>
-						<input type="text" placeholder=""name="jk" id="jk">
-					</div>
-			
-			
-					<div class="input-box">
-						<span class="details">Tinggal Bersama
-						</span>
-						<input type="text" placeholder=""name="tglbsm" id="tglbsm">
-					
-					</div>
-				
-					<div class="input-box">
+						<div class="input-box">
 							<span class="details">NIK</span>
-						<input type="text" placeholder=""name="nik" id="nik">			
-					</div>
+							<input type="text" placeholder=""name="nik" id="nik">			
+						</div>			
 			
+						<div class="input-box">
+							<span class="details">Anak Ke :</span>
+							<input type="text" placeholder=""name="anakke" id="anakke">
+						</div>
 			
-					<div class="input-box">
-						<span class="details">Anak Ke :</span>
-						<input type="text" placeholder=""name="anakke" id="anakke">
-			
-					</div>
-			
-					<div class="input-box">
-						<span class="details">Tempat/Tgl Lahir</span>
-						<input type="text" placeholder=""name="TTL" id="TTL">
-						
-					</div>
+						<div class="input-box">
+							<span class="details">Tempat Lahir</span>
+							<input type="text" placeholder=""name="tempatlahir" id="tempatlahir">	
+						</div>
+
+						<div class="input-box">
+							<span class="details">Tanggal Lahir</span>
+							<input type="text" placeholder=""name="tanggallahir" id="tanggallahir">	
+						</div>
 					
-					<div class="input-box">
-						<span class="details">Usia</span>
-						<input type="text" placeholder=""name="usia" id="usia">
+						<div class="input-box">
+							<span class="details">Usia</span>
+							<input type="text" placeholder=""name="usia" id="usia">
+						</div>
 
+						<div class="input-box">
+							<span class="details">Agama :</span>
+							<input type="text" placeholder=""name="agm" id="agm">
+						</div>	
+
+						<div class="input-box">
+							<span class="details">NO HP :</span>
+							<input type="text" placeholder=""name="nhp" id="nhp">
+						</div>		
+					
+						<div class="input-box">
+							<span class="details">Kewarganegaraan :</span>
+							<input type="text" placeholder=""name="kwg" id="kwg">
+						</div>	
+					
 					</div>
-					<div class="input-box">
-						<span class="details">Agama :</span>
-						<input type="text" placeholder=""name="agm" id="agm">
-
-					</div>	
-					<div class="input-box">
-						<span class="details">
-						NO HP :</span>
-						<input type="text" placeholder=""name="nhp" id="nhp">
-
-					</div>		
-					<div class="input-box">
-						<span class="details">Kewarganegaraan :</span>
-						<input type="text" placeholder=""name="kwg" id="kwg">
-
-					</div>	
-				</div>
 			
         	<div class="title">B. Indentitas Orang Tua / Wali</div>
-        	<br>
-        	<div>1. Ayah Kandung</div>
-        	<div class="user-details">
-		    		<div class="input-box">
-					  <span class="details">Nama Ayah </span>
-					  <input type="text" placeholder=""name="namaayah" id="namaayah">
-		    		</div>
+        		<br>
+
+        		<div>1. Ayah Kandung</div>
+        			<div class="user-details">
+			    			<div class="input-box">
+						  		<span class="details">Nama Ayah </span>
+						 			<input type="text" placeholder=""name="namaayah" id="namaayah">
+			    			</div>
+					
+								<div class="input-box">
+									<span class="details">Pendidikan Ayah </span>
+									<input type="text" placeholder="" name="pdkayah" id="pdkayah">
+								</div>
+					
+								<div class="input-box">
+									<span class="details">NIK</span>
+									<input type="text" placeholder="" name="nikayah" id="nikayah">
+								</div>
+					
+								<div class="input-box">
+										<span class="details">Pekerjaan Ayah </span>
+									<input type="text" placeholder=""name="pkjayah" id="pkjayah">			
+								</div>
+					
+								<div class="input-box">
+									<span class="details">Tanggal lahir :</span>
+									<input type="text" placeholder="" name="tglayah" id="tglayah">
+								</div>			
+						</div>
+
+
+						<div>2. Ibu Kandung</div>
+        			<div class="user-details">
+								
+							<div class="input-box">
+					  		<span class="details">Nama Ibu </span>
+					  		<input type="text" placeholder="" name="namaibu" id="namaibu">
+		    			</div>
 		    
-					<div class="input-box">
-				
-						<span class="details">Pendidikan Ayah </span>
-						<input type="text" placeholder="" name="pdkayah" id="pdkayah">
-						
-					</div>
+							<div class="input-box">
+								<span class="details">Pendidikan Ibu </span>
+								<input type="text" placeholder="" name="pdkibu" id="pdkibu">
+							</div>
 			
-					<div class="input-box">
-						<span class="details">NIK</span>
-						<input type="text" placeholder="" name="nikayah" id="nikayah">
-					</div>
+							<div class="input-box">
+								<span class="details">NIK</span>
+								<input type="text" placeholder="masukan tanggal lahir"name="nikibu" id="nikibu">
+							</div>
 			
+							<div class="input-box">
+									<span class="details">Pekerjaan Ibu :</span>
+								<input type="text" placeholder=""name="pkjibu" id="pkjibu">			
+							</div>
 			
-					<div class="input-box">
-							<span class="details">Pekerjaan Ayah </span>
-						<input type="text" placeholder=""name="pkjayah" id="pkjayah">			
-					</div>
-			
-					<div class="input-box">
-						<span class="details">Tanggal lahir :
-						</span>
-						<input type="text" placeholder="" name="tglayah" id="tglayah">
-					
-					</div>
-			
-				</div>
-				<div>2. Ibu Kandung</div>
-        	<div class="user-details">
-		    		<div class="input-box">
-					  <span class="details">Nama Ibu </span>
-					  <input type="text" placeholder="" name="namaibu" id="namaibu">
-		    		</div>
-		    
-					<div class="input-box">
-				
-						<span class="details">Pendidikan Ibu </span>
-						<input type="text" placeholder="" name="pdkibu" id="pdkibu">
-						
-					</div>
-			
-					<div class="input-box">
-						<span class="details">NIK</span>
-						<input type="text" placeholder="masukan tanggal lahir"name="nikibu" id="nikibu">
-					</div>
-			
-			
-					<div class="input-box">
-							<span class="details">Pekerjaan Ibu :</span>
-						<input type="text" placeholder=""name="pkjibu" id="pkjibu">			
-					</div>
-			
-					<div class="input-box">
-						<span class="details">Tanggal Lahir
-						</span>
-						<input type="text" placeholder="" name="tglibu" id="tglibu">
-					
-					</div>
+							<div class="input-box">
+								<span class="details">Tanggal Lahir
+								</span>
+								<input type="text" placeholder="" name="tglibu" id="tglibu">
+							</div>
 				
 					
-				</div>
-				<div class="title">C. PERIODIK</div>
-        	<br>
+						</div>
+				
+					<div class="title">C. PERIODIK</div>
+        		<br>
         	
-        	<div class="user-details">
-		    		<div class="input-box">
-					  <span class="details">Tinggi Badan </span>
-					  <input type="text" placeholder="" name="tgbdn" id="tgbdn">
-		    		</div>
+        		<div class="user-details">
+		    			<div class="input-box">
+					  		<span class="details">Tinggi Badan </span>
+					  		<input type="text" placeholder="" name="tgbdn" id="tgbdn">
+		    			</div>
 		    
-					<div class="input-box">
-				
-						<span class="details">Jarak Tempuh </span>
-						<input type="text" placeholder="" name="jktp" id="jktp">
-						
-					</div>
+							<div class="input-box">
+								<span class="details">Jarak Tempuh </span>
+								<input type="text" placeholder="" name="jktp" id="jktp">
+							</div>
 			
-					<div class="input-box">
-						<span class="details">Berat Badan</span>
-						<input type="text" placeholder="" name="btbdn" id="btbdn">
-					</div>
+							<div class="input-box">
+								<span class="details">Berat Badan</span>
+								<input type="text" placeholder="" name="btbdn" id="btbdn">
+							</div>
 			
-			
-					<div class="input-box">
-							<span class="details">Jumlah Saudara </span>
-						<input type="text" placeholder=""name="js" id="js">			
-					</div>
-			
-					
-				
-				</div>
+							<div class="input-box">
+									<span class="details">Jumlah Saudara </span>
+								<input type="text" placeholder=""name="js" id="js">			
+							</div>
+						</div>
+
 					<div class="title"> D.Register</div>
-				 <br>
-					<div class="gender-details">
-	          <input type="radio" name="gender" id="dot-1">
-	          <input type="radio" name="gender" id="dot-2">
-	          <input type="radio" name="gender" id="dot-3">
-	          <span class="gender-title">Jenis Pendaftaran</span>
-          <div class="category">
-            <label for="dot-1">
-            <span class="dot one"></span>
-            <span class="gender">Siswa Baru</span>
-          </label>
-          <label for="dot-2">
-            <span class="dot two"></span>
-            <span class="gender">Pindahan</span>
-          </label>
-          <label for="dot-3">
-            <span class="dot three"></span>
-            <span class="gender">Sekolah Lagi</span>
-            </label>
-          </div> 
-        </div>
-        <br>
+				 		<br>
+							<div class="gender-details">
+			          <input type="radio" name="jenis" id="jenis1" value="Siswa Baru">
+			          <input type="radio" name="jenis" id="jenis2" value="Pindahan">
+			          <input type="radio" name="jenis" id="jenis3" value="Sekolah Lagi">
+			          <span class="gender-title">Jenis Pendaftaran</span>
+         			<div class="category">
+            		<label for="dot-1">
+            			<span class="dot one"></span>
+            			<span class="gender">Siswa Baru</span>
+          			</label>
+			          <label for="dot-2">
+			            <span class="dot two"></span>
+			            <span class="gender">Pindahan</span>
+			          </label>
+			          <label for="dot-3">
+			            <span class="dot three"></span>
+			            <span class="gender">Sekolah Lagi</span>
+			          </label>
+          		</div> 
+        	</div>
+        		<br>
 
         <div class="rombel"> Rombel </div>
-        <div class="gender-details">
-        	<input type="checkbox" name="gendor" id="dot1">
-        	<label>Kelompok A</label>
-        	<br>
-        	<input type="checkbox" name="gender" id="dot2">
-        	<label>Kelompok B</label>
-				</div>
-       <!--  	<div class="kategori">
-        		<label for="dot1">
-        			<span class="dot satu"></span>
-        			<span class="gender">Kelompok A</span>	
-        		</label>
-        		<label for="dot2">
-        			<span class="dot dua"></span>
-        			<span class="gender">Kelompok B</span>	
-        		</label>
-        	</div>
-        </div> -->
+        	<div class="gender-details">
+	        	<input type="checkbox" name="rombel" id="dot1" value="Kelompok A">
+	        	<label>Kelompok A</label>
+	        	<br>
+	        	<input type="checkbox" name="rombel" id="dot2" value="Kelompok B">
+	        	<label>Kelompok B</label>
+					</div>
 
-        <div class="user-details">
+        	<div class="user-details">
 		    		<div class="input-box">
 					  <span class="details">Tanggal masuk sekolah </span>
 					  <input type="text" placeholder="" name="tglmskskl" id="tglmskskl">
-		    		</div>
+		    	</div>
 		    
 					<div class="input-box">
 						<span class="details">No Induk Siswa </span>
 						<input type="text" placeholder="" name="nis" id="nis">
 					</div>
-				</div>
-				
-			
-					<div class="button">
-    				<input type="button" onclick="location.href='afterppdb.php';" value="SIMPAN" />
-						</form>	
-						</form>
-        			</div>
+					</div>
+
+					<!-- Button untuk menyimpan -->
+        	<div class="button">
+        		<button type="submit" name="ppdb">Simpan</button>
+        	</div>
 		
 		  	</form>	
-		  	
+		  </form>	
 		</div>
-	
- </div>
-	
-</body>
+ 	</div>
+	</body>
 </html>
